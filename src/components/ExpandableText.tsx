b64313c0b407c0b9928be4c97bf6dd136ec9b45e
Yusein Kerdakov <yu.kerdakov@proton.me>
2023-06-22 16:02:40 +0300

    handle truncate

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -10,15 +10,15 @@ const ExpandableText = ({ children, maxChars = 516 }: Props) => {
 
   const showedText = children[0].substring(0, maxChars);
   const text = isExpanded ? children : showedText;
- 
-    const buttonStyle = {
-      color: "#6237f0",
-      borderRadius: "5px",
-    };
+
+  const buttonStyle = {
+    color: "#6237f0",
+    borderRadius: "5px",
+  };
 
   return (
     <div className="aboutInfo">
-      {text} ...
+      {`${text}${!isExpanded ? "..." : ""} `}
       <button style={buttonStyle} onClick={() => setExpanded(!isExpanded)}>
         {isExpanded ? "По-малко" : "Повече"}
       </button>
